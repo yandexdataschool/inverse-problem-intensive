5ba43a2c621d93fe3623a2a7d65fe7d6c42364cf
Nikita Kazeev <nikita.kazeev@cern.ch>
2021-04-19 07:37:32 +0000

    Added naive & SNPE pendullum inference

--- pendulum.py
+++ pendulum.py
@@ -1,5 +1,6 @@
 # Adapted from https://colab.research.google.com/drive/1CSy-xfrnTX28p1difoTA8ulYw0zytJkq#scrollTo=srZU0YiAQ8rm
 from functools import partial
+from matplotlib.patches import Circle
 import jax
 import jax.numpy as jnp
 from jax.experimental.ode import odeint
@@ -37,14 +38,14 @@ def f_analytical(state, t=0, m1=1, m2=1, l1=1, l2=1, g=9.8):
 @partial(jax.jit, backend='cpu')
 def solve_autograd(initial_state, times, m1=1, m2=1, l1=1, l2=1, g=9.8):
   L = partial(lagrangian, m1=m1, m2=m2, l1=l1, l2=l2, g=g)
-  return solve_lagrangian(L, initial_state, t=times, rtol=1e-10, atol=1e-10)
+  return solve_lagrangian(L, initial_state, t=times)
 
 
 # Double pendulum dynamics via analytical forces taken from Diego's blog
 @partial(jax.jit, backend='cpu')
 def solve_analytical(initial_state, times, m1=1, m2=1, l1=1, l2=1, g=9.8):
   f_parametrised = partial(f_analytical, m1=m1, m2=m2, l1=l1, l2=l2, g=g)
-  return odeint(f_parametrised, initial_state, t=times, rtol=1e-10, atol=1e-10)
+  return odeint(f_parametrised, initial_state, t=times)
 
 
 def normalize_dp(state):
@@ -59,3 +60,49 @@ def rk4_step(f, x, t, h):
   k3 = h * f(x + k2/2, t + h/2)
   k4 = h * f(x + k3, t + h)
   return x + 1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+
+
+def make_plot(i, cart_coords, l1, l2, max_trail=30, trail_segments=20, r = 0.05):
+    # Plot and save an image of the double pendulum configuration for time step i.
+    plt.cla()
+
+    x1, y1, x2, y2 = cart_coords
+    ax.plot([0, x1[i], x2[i]], [0, y1[i], y2[i]], lw=2, c='k') # rods
+    c0 = Circle((0, 0), r/2, fc='k', zorder=10) # anchor point
+    c1 = Circle((x1[i], y1[i]), r, fc='b', ec='b', zorder=10) # mass 1
+    c2 = Circle((x2[i], y2[i]), r, fc='r', ec='r', zorder=10) # mass 2
+    ax.add_patch(c0)
+    ax.add_patch(c1)
+    ax.add_patch(c2)
+
+    # plot the pendulum trail (ns = number of segments)
+    s = max_trail // trail_segments
+    for j in range(trail_segments):
+        imin = i - (trail_segments-j)*s
+        if imin < 0: continue
+        imax = imin + s + 1
+        alpha = (j/trail_segments)**2 # fade the trail into alpha
+        ax.plot(x2[imin:imax], y2[imin:imax], c='r', solid_capstyle='butt',
+                lw=2, alpha=alpha)
+
+    # Center the image on the fixed anchor point. Make axes equal.
+    ax.set_xlim(-l1-l2-r, l1+l2+r)
+    ax.set_ylim(-l1-l2-r, l1+l2+r)
+    ax.set_aspect('equal', adjustable='box')
+    plt.axis('off')
+    
+
+def radial2cartesian(t1, t2, l1, l2):
+  # Convert from radial to Cartesian coordinates.
+  x1 = l1 * np.sin(t1)
+  y1 = -l1 * np.cos(t1)
+  x2 = x1 + l2 * np.sin(t2)
+  y2 = y1 - l2 * np.cos(t2)
+  return x1, y1, x2, y2
+
+
+def fig2image(fig):
+  fig.canvas.draw()
+  data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+  image = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+  return image
